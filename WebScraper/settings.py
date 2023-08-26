2953703a210f39b9c09eaa948356f55afe7096af
Grigor Radkov <grigorrradkov@gmail.com>
2023-08-27 00:33:02 +0300

    Added splash for rendering js

--- WebScraper/settings.py
+++ WebScraper/settings.py
@@ -12,6 +12,21 @@ BOT_NAME = "WebScraper"
 SPIDER_MODULES = ["WebScraper.spiders"]
 NEWSPIDER_MODULE = "WebScraper.spiders"
 
+#Splash configuration
+SPLASH_URL = 'http://localhost:8050/'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "WebScraper (+http://www.yourdomain.com)"
--- WebScraper/spiders/orb_weaver.py
+++ WebScraper/spiders/orb_weaver.py
@@ -1,20 +1,31 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 class OrbWeaver(scrapy.Spider):
     name = "orb_weaver"
-    allowed_domains = ["shop.mango.com"]
+    #allowed_domains = ["shop.mango.com"]
     start_urls = [
         "https://shop.mango.com/bg-en/men/t-shirts-plain/100-linen-slim-fit-t-shirt_47095923.html?c=07",
     ]
 
+    def start_requests(self):
+        
+        for url in self.start_urls:
+            yield scrapy.Request(url = url, callback = self.parse)
+            #yield SplashRequest(url = url, callback = self.parse)
+
+
     def parse(self, response):
         self.logger.info("Response from %s", response.url)
-        name   = response.css("title::text").extract()
-        script  = response.css("script").re_first(r"\"salePrice\":[0-9]+.[0-9]+")
-        #colour = response.css("micro-frontend").extract()
-        #size   = response.css("title").extract()
+        #name   = response.css("title::text").extract()
+        #script  = response.css("script").re_first(r"\"salePrice\":([0-9]+.[0-9]+)")
+        #colour = response.css("script").extract(r"\"salePrice\":([0-9]+.[0-9]+)")
+        #size   = response.css("script").extract(r"\"sizeAvailability\":([a-Z]+.[a-Z]+)")
+        #price = response.css("span").extract()
 
-        yield {"name" : name}
-        yield {"script" : script}
-        #yield {"colour" : colour}
-        
+        #yield   {"name" : name,
+        #        "script" : script,
+        #        "price" : price
+        #        }
+        with open('page.html', 'wb') as html_file:
+            html_file.write(response.body)
